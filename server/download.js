32bef908d73da62c91c086ddde231e7b718de80e
paulameg <paulatakahashi4@gmail.com>
2023-09-18 19:49:23 -0300

    NLW-AI

---
+++ server/download.js
@@ -0,0 +1,32 @@
+import ytdl from "ytdl-core";
+import fs from 'fs';
+
+export const download = (videoID) => new Promise ((resolve, reject) => {
+    const videoURL = "https://www.youtube.com/shorts/" + videoID;
+    console.log("Realizando o download do vídeo:", videoID);
+
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly"}).on(
+        "info",
+        (info) => {
+            const seconds = info.formats[0].approxDurationMs / 1000
+            
+            if(seconds > 60) {
+                throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+            }
+            console.log(seconds);
+        }
+    )
+    .on("end", () => {
+        console.log("Download do vídeo finalizado");
+        resolve();
+    })
+    .on("error", (error) => {
+        console.log("Não foi possível fazer o download do vídeo. Detalhes do erro:", erro);
+        reject(error);
+    })
+    .pipe(fs.createWriteStream("./tmp/audio.mp4"));
+
+
+
+})
+
